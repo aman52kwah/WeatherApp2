c2782d0ee2626acbc510cf0299d49f8436864b17
aman52kwah <amankwaamichael52@gmail.com>
2025-04-15 22:03:43 +0100

    add weather update

--- app.js
+++ app.js
@@ -41,6 +41,7 @@ try {
     if(!response.ok) throw new Error('city not found');
     const data = await response.json();
     updateWeatherUI(data);
+    errorMsg.classList.add('hidden');
 } catch (error) {
     showError(error.message);
 }
@@ -56,10 +57,49 @@ searchBtn.addEventListener('click',() =>{
 //update weather UI
 function updateWeatherUI(data){
     const {name,sys,main,weather,wind,rain}= data;
+   
+   //update main weather
     tempElement.textContent=`${Math.round(main.temp)}°C`;
     descriptionElement.textContent=weather[0].description;
     locationElement.textContent =`${name}, ${sys.country}`;
 
     //update weather UI details
-    
+    humidityElement.textContent=`${main.humidity}%`;
+    windElement.textContent =`${Math.round(wind.speed * 3.6)}km/h`;
+
+
+    //percipitation if rain might not be available and fixed to mm
+    const percipitation = rain ? rain ['1h'] || 0 : 0 ;
+    precipitationElement.textContent =`${percipitation}mm`;
+
+
+    //update Date
+    updateDate();
+
+    //upate weather icon
+    if (weatherIcon) {
+        weatherIcon.innerHTML=`<img src="https://openweathermap.org/img/wn/${weather[0].icon}.png" alt="${
+        weather[0].description}">`;
+    }
+}
+
+function updateDate(){
+    const now = new Date();
+    const days =['Sunday','Monday','TUESDAY','Wednesday',
+        'Thursday','Friday','Saturday'
+    ];
+    const months = ['Jan','Feb','Mar','Apr','May','June','Jul',
+        'Aug','Sep','Oct','Nov','Dec'
+    ];
+
+    dateDayName.textContent =days[now.getDay];
+    dateDay.textContent=`${now.getDate()} ${months[now.getMonth()]} ${now.getFullYear()}`;
+}
+
+//show error message if city not found
+
+
+function showError(message){
+errorMsg.textContent= message;
+errorMsg.classList.remove('hidden');
 }
